56ba9e71d7b50aa7237ae67c24437af6ab987f6b
ayon121 <borshonsaha1234@gmail.com>
2025-10-04 22:07:14 +0600

    dashboard page updated

--- src/app/(public)/dashboard/page.tsx
+++ src/app/(public)/dashboard/page.tsx
@@ -1,16 +1,22 @@
 import { authOptions } from "@/helpers/authOptions";
 import { getServerSession } from "next-auth";
 
+const DashboardPage = async () => {
+  const session = await getServerSession(authOptions);
+  const username = session?.user?.name || "User";
 
-const Dashboardpage = async() => {
-    const session = await getServerSession(authOptions)
-
-    console.log(session);
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    );
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center text-gray-800">
+      <div className="text-center px-4">
+        <h1 className="text-4xl font-semibold mb-2">
+          Welcome back, <span className="text-main">{username}</span> 👋
+        </h1>
+        <p className="text-lg text-gray-600 mt-2">
+          Manage your projects, blogs, and personal updates all from one place.
+        </p>
+      </div>
+    </main>
+  );
 };
 
-export default  Dashboardpage;
+export default DashboardPage;
